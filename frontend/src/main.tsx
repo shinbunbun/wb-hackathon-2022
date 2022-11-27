0594eefce56b47a573ab50fe35827107cce791bf
eraser5th <83107074+eraser5th@users.noreply.github.com>
2022-11-27 19:14:40 +0900

    setting path alias

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import Helmet from 'react-helmet-async'
 import 'the-new-css-reset/css/reset.css'
+import { AppRouter } from '@/Router'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>

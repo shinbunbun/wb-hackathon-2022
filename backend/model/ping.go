f93d85fb92a195447d7b223f18e4f9543a30b340
shinbunbun <takamuneyuto@gmail.com>
2022-11-27 18:19:35 +0900

    modelのファイルを修正

--- backend/model/ping.go
+++ backend/model/ping.go
@@ -3,14 +3,3 @@ package model
 type Ping struct {
 	Message string `json:"message"`
 }
-
-type MemoryPostReq struct {
-	ImageUrl string `json:"imageUrl"`
-	Label    string `json:"label"`
-}
-
-type Empty struct{}
-
-type MemoryGetRes struct {
-	ImageUrls []string `json:"imageUrls"`
-}

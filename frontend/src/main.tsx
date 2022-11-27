aaeb3b237769b0d30d271e7afb13daff8d4c82df
eraser5th <83107074+eraser5th@users.noreply.github.com>
2022-11-27 19:08:53 +0900

    setting react-helmet-async

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import Helmet from 'react-helmet-async'
 import { AppRouter } from './Router'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <AppRouter />
+    <Helmet.HelmetProvider>
+      <AppRouter />
+    </Helmet.HelmetProvider>
   </React.StrictMode>
 )

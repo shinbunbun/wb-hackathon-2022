4979cca70abd869149f236bf27ed0d43ae2f2846
eraser5th <83107074+eraser5th@users.noreply.github.com>
2022-11-27 19:23:04 +0900

    setting vite-plugin-svgr

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,10 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import { vanillaExtractPlugin } from '@vanilla-extract/vite-plugin';
+import svgr from 'vite-plugin-svgr'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), vanillaExtractPlugin()],
+  plugins: [react(), vanillaExtractPlugin(), svgr()],
   resolve: {
     alias: {
       "@/": "/src/"

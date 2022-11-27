0980478db402d964168f0a22bd900c13053b56b6
shinbunbun <takamuneyuto@gmail.com>
2022-11-27 17:59:43 +0900

    modelを作成

--- backend/model/ping.go
+++ backend/model/ping.go
@@ -3,3 +3,14 @@ package model
 type Ping struct {
 	Message string `json:"message"`
 }
+
+type MemoryPostReq struct {
+	ImageUrl string `json:"imageUrl"`
+	Label    string `json:"label"`
+}
+
+type Empty struct{}
+
+type MemoryGetRes struct {
+	ImageUrls []string `json:"imageUrls"`
+}

77515f03307fd7106886204df50407f240cc6ae6
eraser5th <83107074+eraser5th@users.noreply.github.com>
2022-11-27 18:52:21 +0900

    setting routing

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { App } from './App'
+import { AppRouter } from './Router'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <AppRouter />
   </React.StrictMode>
 )

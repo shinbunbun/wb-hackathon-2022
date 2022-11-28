84f42e877f23d0871537b87320996c7f0794c819
eraser5th <83107074+eraser5th@users.noreply.github.com>
2022-11-28 16:11:34 +0900

    fix style

--- frontend/src/pages/FileUploadPage.tsx
+++ frontend/src/pages/FileUploadPage.tsx
@@ -1,14 +1,20 @@
+import { Page } from '@/util/components/Page'
+
 export const FileUploadPage: React.FC<{
   image: File
   submit: () => void
 }> = ({ image, submit }) => {
   const imageUrl = URL.createObjectURL(image)
   return (
-    <div>
-      <img src={imageUrl} alt="preview" />
+    <Page>
+      <img
+        style={{ width: '300px', height: '300px', objectFit: 'cover' }}
+        src={imageUrl}
+        alt="preview"
+      />
       <button type="button" onClick={submit}>
         アップロードする
       </button>
-    </div>
+    </Page>
   )
 }

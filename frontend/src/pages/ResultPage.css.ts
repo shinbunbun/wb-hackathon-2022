5ab8b5b5f6fc21cc672addfd25435a1a7363aded
eraser5th <83107074+eraser5th@users.noreply.github.com>
2022-11-28 15:41:11 +0900

    fix

--- frontend/src/pages/ResultPage.css.ts
+++ frontend/src/pages/ResultPage.css.ts
@@ -1,8 +1,8 @@
-import { style } from "@vanilla-extract/css";
+import { style } from '@vanilla-extract/css'
 
 export const gallery = style({
-  display: "grid",
-  gridTemplateColumns: "repeat(3, calc(calc(100% - 32) / 3))"
+  display: 'grid',
+  gridTemplateColumns: 'repeat(3, calc(calc(100% - 32) / 3))',
 })
 
 export const galleryItem = style({
